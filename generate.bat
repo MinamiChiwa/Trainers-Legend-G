3a96185ae92057dce085c255f891038d1d94e744
akemimadoka <chino@hotococoa.moe>
2024-04-21 20:03:41 +0800

    conan 尝试使用旧版库

--- generate.bat
+++ generate.bat
@@ -1,3 +1,3 @@
 @echo off
-conan install . -if build -s build_type=Release --build missing
+conan install . -if build -s build_type=Release -s compiler.version=16 --build missing
 utils\bin\premake5 %* vs2022
